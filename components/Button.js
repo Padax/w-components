b61270e3b6146adeefcaa7b48c414f4f34531941
cwpeng <padalab@gmail.com>
2021-07-09 03:22:52 +0800

    Create Global Theme.CSS

--- components/Button.js
+++ components/Button.js
@@ -3,50 +3,50 @@ const stylesheet=`
   button{
     font-size:1rem;
     display:inline-block;
-    border-width:0px;border-color:var(--primary-color);border-style:solid;
-    background-color:var(--primary-color);color:#eeeeee;
+    border-width:0px;border-color:var(--primary-color-60);border-style:solid;
+    background-color:var(--primary-color-60);color:#eeeeee;
     padding:8px 10px;
     cursor:pointer;transition:background-color 0.5s;
   }
   button:hover, button:active{
-    background-color:#140096;
+    background-color:var(--primary-color-40);
   }
-  button.success{
-    background-color:#11ae11;
-    border-color:#11ae11;
+  button.critical{
+    background-color:var(--critical-color-60);;
+    border-color:var(--critical-color-60);;
   }
-  button.success:hover, button.success:active{
-    background-color:#156715;
+  button.critical:hover, button.critical:active{
+    background-color:var(--critical-color-40);;
   }
   /* disabled */
   button:disabled{
     opacity:0.5;
   }
   button:disabled:hover, button:disabled:active{
-    background-color:var(--primary-color);
+    background-color:var(--primary-color-60);
   }
-  button.success:disabled:hover, button.success:disabled:active{
-    background-color:#11ae11;
+  button.critical:disabled:hover, button.critical:disabled:active{
+    background-color:var(--critical-color-60);;
   }
   /* outline */
   button.outline-primary{
     background-color:transparent;
-    border-color:var(--primary-color);
+    border-color:var(--primary-color-60);
     border-width:1px;
-    color:var(--primary-color);
+    color:var(--primary-color-60);
   }
   button.outline-primary:hover, button.outline-primary:active{
-    background-color:#140096;
+    background-color:var(--primary-color-40);
     color:#eeeeee;
   }
-  button.outline-success{
+  button.outline-critical{
     background-color:transparent;
-    border-color:#156715;
+    border-color:var(--critical-color-40);;
     border-width:1px;
-    color:#11ae11;
+    color:var(--critical-color-60);;
   }
-  button.outline-success:hover, button.outline-success:active{
-    background-color:#156715;
+  button.outline-critical:hover, button.outline-critical:active{
+    background-color:var(--critical-color-40);;
     color:#eeeeee;
   }
   /* size */
@@ -63,7 +63,6 @@ class Button extends HTMLElement{
   constructor(){
     super();
     const shadowRoot=this.attachShadow({mode:"closed"});
-    DOM.create("style", {props:{textContent:document.querySelector("#global-for-local").textContent}}, shadowRoot);
     DOM.create("style", {props:{textContent:stylesheet}}, shadowRoot);
     // prepare all attributes
     const attrs={};
--- wc.js
+++ wc.js
@@ -1,3 +1,4 @@
+import DOM from "./components/util/DOM.js";
 import Nav from "./components/Nav.js";
 import Link from "./components/Link.js";
 import Button from "./components/Button.js";
@@ -6,12 +7,18 @@ import Calendar from "./components/Calendar.js";
 import Heading from "./components/Heading.js";
 const wc={
   init:function(prefix="wc"){
+    this.initTheme();
     window.customElements.define(prefix+"-nav", Nav);
     window.customElements.define(prefix+"-link", Link);
     window.customElements.define(prefix+"-button", Button);
     window.customElements.define(prefix+"-dialog", Dialog);
     window.customElements.define(prefix+"-calendar", Calendar);
     window.customElements.define(prefix+"-heading", Heading);
+  },
+  initTheme:function(){
+    DOM.create("link", {props:{
+      rel:"stylesheet", type:"text/css", href:"./components/theme/light.css"
+    }}, document.querySelector("head"));
   }
 };
 export default wc;
