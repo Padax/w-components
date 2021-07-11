49c435e78bf68b78296c3a713a6af39abcbc0c2b
Niz <nizniz187@gmail.com>
2021-07-12 02:09:41 +0800

    Resolved issue #16: Add Quote component

---
+++ components/Quote.js
@@ -0,0 +1,54 @@
+import DOM from "./util/DOM.js";
+const stylesheet = `
+  .left { text-align: left; }
+  .center { text-align: center; }
+  .right { text-align: right; }
+  .source {
+    font-size: var(--font-size-small);
+    line-height: calc(var(--font-size-small) * var(--line-height-ratio));
+    color: var(--color-gray-60);
+  }
+  .source:before {
+    content: '— '
+  }
+`;
+class Quote extends HTMLElement{
+  constructor(){
+    super();
+    this.attachShadow({ mode: 'open' });
+    this.render();
+  }
+  static defaultValues = {
+    align: 'left'
+  };
+
+  getDefaultValueByName(name) {
+    return this.constructor.defaultValues[name];
+  }
+  /**
+   * Parse align attribute to a valid value
+   * @param {string} align 
+   * @returns {string} Alignment class name
+   */
+   parseAlign(align = this.getAttribute('align')) {
+    if(align == 'left' || align == 'center' || align == 'right') {
+      return align;
+    }
+    return this.getDefaultValueByName('align');
+  }
+
+  render() {
+    DOM.create('style', { props: { textContent: stylesheet } }, this.shadowRoot);
+
+    const container = DOM.create('div', { props: { className: this.parseAlign() } }, this.shadowRoot);
+    
+    const quoteContainer = DOM.create('div', {}, container);
+    DOM.create('slot', { props: { name: 'quote' } }, quoteContainer);
+
+    if(this.querySelector('[slot="source"]')) {
+      const sourceContainer = DOM.create('div', { props: { className: 'source' } }, container);
+      DOM.create('slot', { props: { name: 'source' } }, sourceContainer);
+    }
+  }
+}
+export default Quote;
--- wc.js
+++ wc.js
@@ -6,6 +6,7 @@ import Dialog from "./components/Dialog.js";
 import Calendar from "./components/Calendar.js";
 import Heading from "./components/Heading.js";
 import DisplayHeading from "./components/DisplayHeading.js";
+import Quote from "./components/Quote.js";
 const wc={
   init:function(prefix="wc"){
     this.initTheme();
@@ -16,6 +17,7 @@ const wc={
     window.customElements.define(prefix+"-calendar", Calendar);
     window.customElements.define(prefix+"-heading", Heading);
     window.customElements.define(prefix+"-display-heading", DisplayHeading);
+    window.customElements.define(prefix+"-quote", Quote);
   },
   initTheme:function(){
     DOM.create("link", {props:{
