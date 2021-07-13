3b3fc025648bf6a04427cae93b57664149ddfa59
cwpeng <padalab@gmail.com>
2021-07-13 17:56:12 +0800

    Replace Link Component by Styled A Tag

--- wc.js
+++ wc.js
@@ -1,6 +1,5 @@
 import DOM from "./components/util/DOM.js";
 import Nav from "./components/Nav.js";
-import Link from "./components/Link.js";
 import Button from "./components/Button.js";
 import Dialog from "./components/Dialog.js";
 import Calendar from "./components/Calendar.js";
@@ -11,7 +10,6 @@ const wc={
   init:function(prefix="wc"){
     this.initTheme();
     window.customElements.define(prefix+"-nav", Nav);
-    window.customElements.define(prefix+"-link", Link);
     window.customElements.define(prefix+"-button", Button);
     window.customElements.define(prefix+"-dialog", Dialog);
     window.customElements.define(prefix+"-calendar", Calendar);
