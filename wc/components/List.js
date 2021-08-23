db239c10284168940f3515ac5b9091c86a8c4eca
cwpeng <padalab@gmail.com>
2021-08-24 03:07:22 +0800

    Apply slot for w-list and w-li components #91

--- wc/components/List.js
+++ wc/components/List.js
@@ -1,9 +1,5 @@
 import WComponent, { DOM, AttributeParser } from "../WComponent.js";
-/*
 const stylesheet=`
-  :host{
-    counter-reset:number;
-  }
   div.list{
     margin:1rem 0px;
   }
@@ -11,90 +7,28 @@ const stylesheet=`
     border:1px solid var(--color-gray-20);
     border-radius:4px;
   }
-  div.list>::slotted([slot='item']){
+  div.list>::slotted(*){
     display:flex;align-items:center;
     padding:0.5rem;
   }
-  div.list[appearance='divided-panel']>::slotted([slot='item']){
+  div.list[appearance='divided-panel']>::slotted(*){
     border-bottom:1px solid var(--color-gray-20);
   }
-  div.list[appearance$='panel']>::slotted([slot='item']:hover){
+  div.list[appearance$='panel']>::slotted(*:hover){
     background-color:var(--color-gray-10);
   }
-  div.list[appearance$='panel']>::slotted([slot='item']:active){
+  div.list[appearance$='panel']>::slotted(*:active){
     background-color:var(--color-gray-20);
   }
-  div.list>::slotted([slot='item']:last-child){
-    border-bottom-width:0px;
-  }
-  div.list[mark='circle']>::slotted([slot='item']):before{
-    content:'\\fd2c';
-    font-family:var(--icon-font-filled);
-    margin-right:8px;
-  }
-  div.list[mark='outline-circle']>::slotted([slot='item']):before{
-    content:'\\fd23';
-    font-family:var(--icon-font-regular);
-    margin-right:8px;
-  }
-  div.list[mark='number']>::slotted([slot='item']):before{
-    counter-increment:number;
-    content:counter(number) '.';
-    margin-left:4px;
-    margin-right:8px;
-  }
-`;
-*/
-const stylesheet=`
-  div.list{
-    counter-reset:number;
-    margin:1rem 0px;
-  }
-  div.list[appearance$='panel']{
-    border:1px solid var(--color-gray-20);
-    border-radius:4px;
-  }
-  div.list>.item{
-    display:flex;align-items:center;
-    padding:0.5rem;
-  }
-  div.list[appearance='divided-panel']>.item{
-    border-bottom:1px solid var(--color-gray-20);
-  }
-  div.list[appearance$='panel']>.item:hover{
-    background-color:var(--color-gray-10);
-  }
-  div.list[appearance$='panel']>.item:active{
-    background-color:var(--color-gray-20);
-  }
-  div.list[appearance$='panel']>.item[disabled]{
+  div.list[appearance$='panel']>::slotted(*[disabled]){
     color:var(--color-gray-40);
   }
-  div.list[appearance$='panel']>.item[disabled]:hover,
-  div.list[appearance$='panel']>.item[disabled]:active{
+  div.list[appearance$='panel']>::slotted(*[disabled]:hover),
+  div.list[appearance$='panel']>::slotted(*[disabled]:active){
     background-color:initial;
   }
-  div.list>.item:last-child{
+  div.list>::slotted(*:last-child){
     border-bottom-width:0px;
-  }
-  div.list[mark='circle']>.item>.mark{
-    display:inline-flex;align-items:center;
-  }
-  div.list[mark='circle']>.item>.mark:before{
-    content:'\\fd2c';
-    font-family:var(--icon-font-filled);
-    margin-right:8px;
-  }
-  div.list[mark='outline-circle']>.item>.mark:before{
-    content:'\\fd23';
-    font-family:var(--icon-font-regular);
-    margin-right:8px;
-  }
-  div.list[mark='number']>.item>.mark:before{
-    counter-increment:number;
-    content:counter(number) '.';
-    margin-left:4px;
-    margin-right:8px;
   }
 `;
 class List extends WComponent{
@@ -105,22 +39,22 @@ class List extends WComponent{
   constructor(){
     super();
   }
-  render(){
-    const mark=AttributeParser.parseStringAttr(
+  componentWillRender(){
+    this.mark=AttributeParser.parseStringAttr(
       this.getAttribute("mark"),
       this.getDefaultValueByName("mark"),
       /^none$|^circle$|^outline-circle$|^number$/
     );
+  }
+  render(){
     const appearance=AttributeParser.parseStringAttr(
       this.getAttribute("appearance"),
       this.getDefaultValueByName("appearance"),
       /^normal$|^panel$|^divided-panel$/
     );
-    const attrs={
-      mark, appearance
-    };
-    const list=DOM.create("div", {props:{className:"list"}, attrs:attrs}, this.shadowRoot);
-    // DOM.create("slot", {props:{name:"item"}}, list);
+    const list=DOM.create("div", {props:{className:"list"}, attrs:{appearance}}, this.shadowRoot);
+    DOM.create("slot", {}, list);
+    /*
     const items=this.querySelectorAll(window.prefix+"-li");
     items.forEach((item)=>{
       const markedItemAttrs={};
@@ -131,6 +65,7 @@ class List extends WComponent{
       DOM.create("div", {props:{className:"mark"}}, markedItem);
       DOM.replace(item, markedItem);
     });
+    */
   }
 }
 List.prototype.stylesheet=stylesheet;
