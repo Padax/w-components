b86833d16826f7304adc57ad0e625853b37bd980
cwpeng <padalab@gmail.com>
2021-09-14 13:17:56 +0800

    Styling Sticky Nav #83

--- wc/components/layout/Nav.js
+++ wc/components/layout/Nav.js
@@ -4,7 +4,9 @@ const stylesheet=`
     display:flex;align-items:center;
     padding:15px;
     position:sticky;top:0px;left:0px;
-    background-color:#ffffff;
+    background-color:transparent;
+    transition:background-color 0.2s;
+    z-index:100;
   }
 `;
 class Nav extends WComponent{
@@ -12,7 +14,24 @@ class Nav extends WComponent{
     super();
   }
   init(){
-    const root=DOM.create("slot", {}, this.shadowRoot);
+    this.docTop=document.documentElement.scrollTop;
+    this.docThreshold=100;
+    window.addEventListener("scroll", this.scroll.bind(this));
+    DOM.create("slot", {}, this.shadowRoot);
+  }
+  scroll(){
+    if(this.docTop<this.docThreshold){
+      if(document.documentElement.scrollTop>this.docThreshold){
+        this.style.backgroundColor="white";
+        this.style.borderBottom="1px solid #cccccc";
+      }
+    }else{
+      if(document.documentElement.scrollTop<this.docThreshold){
+        this.style.backgroundColor="transparent";
+        this.style.borderBottom="none";
+      }
+    }
+    this.docTop=document.documentElement.scrollTop;
   }
 }
 Nav.prototype.stylesheet=stylesheet;
