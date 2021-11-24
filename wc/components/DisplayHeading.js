ed5b2b0659e3a72b9544618cd3829b2177ea92b8
Niz <nizniz187@gmail.com>
2021-11-24 23:43:42 +0800

    Implement issue #42: add default margin for Heading & DisplayHeading components.

--- wc/components/DisplayHeading.js
+++ wc/components/DisplayHeading.js
@@ -6,30 +6,35 @@ const stylesheet = `
   h1 { 
     font-size: 5.5rem;
     line-height: calc(5.5rem * var(--line-height-normal-ratio)); 
+    margin-top: 0.318em; margin-bottom: 0.318em;
   }
   h2 { 
     font-size: 5rem;
     line-height: calc(5rem * var(--line-height-normal-ratio)); 
+    margin-top: 0.3em; margin-bottom: 0.3em;
   }
   h3 { 
     font-size: 4.5rem;
     line-height: calc(4.5rem * var(--line-height-normal-ratio)); 
+    margin-top: 0.278em; margin-bottom: 0.278em;
   }
   h4 { 
     font-size: 4rem;
     line-height: calc(4rem * var(--line-height-normal-ratio)); 
+    margin-top: 0.25em; margin-bottom: 0.25em;
   }
   h5 { 
     font-size: 3.5rem; 
     line-height: calc(3.5rem * var(--line-height-normal-ratio)); 
+    margin-top: 0.214em; margin-bottom: 0.214em;
   }
   h6 { 
     font-size: 3rem; 
     line-height: calc(3rem * var(--line-height-normal-ratio)); 
+    margin-top: 0.208em; margin-bottom: 0.208em;
   }
   .heading { 
     font-weight: var(--font-weight-light);
-    margin: 0; 
   }
   .underlined { 
     border-bottom-width: 1px;
--- wc/components/Heading.js
+++ wc/components/Heading.js
@@ -7,33 +7,37 @@ const stylesheet = `
     font-size: 2.5rem; 
     font-weight: var(--font-weight-medium);
     line-height: calc(2.5rem * var(--line-height-normal-ratio)); 
+    margin-top: 0.7em; margin-bottom: 0.7em;
   }
   h2 { 
     font-size: 2rem; 
     font-weight: var(--font-weight-medium);
     line-height: calc(2rem * var(--line-height-normal-ratio));  
+    margin-top: 0.75em; margin-bottom: 0.75em;
   }
   h3 { 
     font-size: 1.5rem; 
     font-weight: var(--font-weight-regular);
     line-height: calc(1.5rem * var(--line-height-normal-ratio)); 
+    margin-top: 0.833em; margin-bottom: 0.833em;
   }
   h4 { 
     font-size: 1.25rem; 
     font-weight: var(--font-weight-regular);
-    line-height: calc(1.25rem * var(--line-height-normal-ratio)); 
+    margin-top: 0.8em; margin-bottom: 0.8em;
   }
   h5 { 
     font-size: 1rem; 
     font-weight: var(--font-weight-regular);
     line-height: calc(1rem * var(--line-height-normal-ratio)); 
+    margin-top: 0.75em; margin-bottom: 0.75em;
   }
   h6 { 
     font-size: .875rem; 
     font-weight: var(--font-weight-regular);
     line-height: calc(.875rem * var(--line-height-normal-ratio)); 
+    margin-top: 1.429em; margin-bottom: 1.429em;
   }
-  .heading { margin: 0; }
   .underlined { 
     border-bottom-width: 1px;
     border-bottom-style: solid;
