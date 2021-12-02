d9f962664cfeb59559c0e7739433f12fdd23519c
Niz <nizniz187@gmail.com>
2021-12-02 19:27:51 +0800

    Resolved issue #132: make Heading & DisplayHeading single-layered

--- wc/components/DisplayHeading.js
+++ wc/components/DisplayHeading.js
@@ -2,50 +2,53 @@ import Heading from "./Heading.js";
 const stylesheet = `
   :host {
     display: block;
+    font-weight: var(--font-weight-light);
   }
-  h1 { 
+`;
+const propStylesheet = {
+  LEVEL1: `
     font-size: 5.5rem;
     line-height: calc(5.5rem * var(--line-height-normal-ratio)); 
     margin-top: 0.318em; margin-bottom: 0.318em;
-  }
-  h2 { 
+  `,
+  LEVEL2: `
     font-size: 5rem;
     line-height: calc(5rem * var(--line-height-normal-ratio)); 
     margin-top: 0.3em; margin-bottom: 0.3em;
-  }
-  h3 { 
+  `,
+  LEVEL3: ` 
     font-size: 4.5rem;
     line-height: calc(4.5rem * var(--line-height-normal-ratio)); 
     margin-top: 0.278em; margin-bottom: 0.278em;
-  }
-  h4 { 
+  `,
+  LEVEL4: `
     font-size: 4rem;
     line-height: calc(4rem * var(--line-height-normal-ratio)); 
     margin-top: 0.25em; margin-bottom: 0.25em;
-  }
-  h5 { 
+  `,
+  LEVEL5: `
     font-size: 3.5rem; 
     line-height: calc(3.5rem * var(--line-height-normal-ratio)); 
     margin-top: 0.214em; margin-bottom: 0.214em;
-  }
-  h6 { 
+  `,
+  LEVEL6: `
     font-size: 3rem; 
     line-height: calc(3rem * var(--line-height-normal-ratio)); 
     margin-top: 0.208em; margin-bottom: 0.208em;
-  }
-  .heading { 
-    font-weight: var(--font-weight-light);
-  }
-  .underlined { 
+  `,
+  UNDERLINED: `
     border-bottom-width: 1px;
     border-bottom-style: solid;
     border-bottom-color: var(--color-gray-20);
-  }
-`;
+  `
+};
 class DisplayHeading extends Heading{
   constructor(){
     super();
   }
 }
 DisplayHeading.prototype.stylesheet=stylesheet;
+DisplayHeading.prototype.propStylesheet=propStylesheet;
+
+
 export default DisplayHeading;
--- wc/components/Heading.js
+++ wc/components/Heading.js
@@ -3,47 +3,49 @@ const stylesheet = `
   :host {
     display: block;
   }
-  h1 { 
+`;
+const propStylesheet = {
+  LEVEL1: `
     font-size: 2.5rem; 
     font-weight: var(--font-weight-medium);
     line-height: calc(2.5rem * var(--line-height-normal-ratio)); 
     margin-top: 0.7em; margin-bottom: 0.7em;
-  }
-  h2 { 
+  `,
+  LEVEL2: `
     font-size: 2rem; 
     font-weight: var(--font-weight-medium);
     line-height: calc(2rem * var(--line-height-normal-ratio));  
     margin-top: 0.75em; margin-bottom: 0.75em;
-  }
-  h3 { 
+  `,
+  LEVEL3: ` 
     font-size: 1.5rem; 
     font-weight: var(--font-weight-regular);
     line-height: calc(1.5rem * var(--line-height-normal-ratio)); 
     margin-top: 0.833em; margin-bottom: 0.833em;
-  }
-  h4 { 
+  `,
+  LEVEL4: `
     font-size: 1.25rem; 
     font-weight: var(--font-weight-regular);
     margin-top: 0.8em; margin-bottom: 0.8em;
-  }
-  h5 { 
+  `,
+  LEVEL5: `
     font-size: 1rem; 
     font-weight: var(--font-weight-regular);
     line-height: calc(1rem * var(--line-height-normal-ratio)); 
     margin-top: 0.75em; margin-bottom: 0.75em;
-  }
-  h6 { 
+  `,
+  LEVEL6: `
     font-size: .875rem; 
     font-weight: var(--font-weight-regular);
     line-height: calc(.875rem * var(--line-height-normal-ratio)); 
     margin-top: 1.429em; margin-bottom: 1.429em;
-  }
-  .underlined { 
+  `,
+  UNDERLINED: `
     border-bottom-width: 1px;
     border-bottom-style: solid;
     border-bottom-color: var(--color-gray-20);
-  }
-`;
+  `
+};
 class Heading extends WComponent{
   static attributes = {
     level: {
@@ -68,12 +70,28 @@ class Heading extends WComponent{
   }
 
   init() {
-    const props = {
-      className: `heading${this.underlined ? ' underlined' : ''}`
-    };
-    const head=DOM.create(`h${this.level}`, { props }, this.shadowRoot);
-    DOM.create("slot", {}, head);
+    console.log(this.getStylesheet())
+    this.setStylesheet(this.getStylesheet(), 'heading');
+    DOM.create("slot", {}, this.shadowRoot);
+  }
+  update({ name, newValue}) {
+    const value = this.parseAttributeValueByName(name, newValue);
+    this.setStylesheet(this.getStylesheet({ [name]: value }), 'heading');
+  }
+
+  getStylesheet({ level, underlined } = {}) {
+    level = level ? level : this.level;
+    underlined = underlined === undefined ? this.underlined : underlined;
+    return `
+      :host {
+        ${this.propStylesheet[`LEVEL${level}`]}
+        ${underlined ? this.propStylesheet.UNDERLINED : ''}
+      }
+    `;
   }
 }
 Heading.prototype.stylesheet=stylesheet;
+Heading.prototype.propStylesheet=propStylesheet;
+
+
 export default Heading;
