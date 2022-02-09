aa6a9e1a67652e698da65a709e2438190b5bd7e2
cwpeng <padalab@gmail.com>
2022-02-09 20:49:57 +0800

    Start Building Dialog Component #25

--- wc/components/dialog/Dialog.js
+++ wc/components/dialog/Dialog.js
@@ -1,27 +1,48 @@
 import WComponent, { DOM, AttributeParser } from "../../WComponent.js";
 const stylesheet=`
   .dialog{
-    width:300px;
-    position:fixed;left:calc(50% - 150px);top:100px;
-    z-index:10;
+    width:600px;
+    position:fixed;left:calc(50% - 300px);top:100px;
+    z-index:10;border-radius:8px;
+    background-color:var(--color-gray-0);
+    box-shadow:var(--box-shadow-30);
   }
   .dialog>.head{
-    background-color:var(--color-gray-80);
-    color:var(--color-gray-10);padding:10px;
+    display:flex;align-items:center;
+    padding:14px 24px;
+    border-bottom:1px solid var(--color-gray-30);
   }
-  .dialog>.head-empty{
-    padding:5px;
+  .dialog>.head>.title{
+    flex:auto;
+    margin:0px;font-weight:bold;
+  }
+  .dialog>.head>.close{
+    flex:none;
+    display:flex;justify-content:center;align-items:center;
+    width:28px;height:28px;cursor:pointer;
+    font-family:var(--icon-font-regular);
+  }
+  .dialog>.head>.close:before{
+    content:'\\f36b';
+    font-size:28px;
   }
   .dialog>slot{
-    display:block;padding:10px;
-    background-color:var(--color-gray-10);
+    display:block;
+    padding:24px;
+    border-bottom:1px solid var(--color-gray-30);
   }
   .dialog>.footer{
-    background-color:var(--color-gray-10);
-    padding:10px;text-align:right;
+    display:flex;align-items:center;
+    padding:24px;
   }
-  .dialog>.footer>w-button{
-    margin-left:10px;
+  .dialog>.footer>.left{
+    flex:auto;
+  }
+  .dialog>.footer>.right{
+    flex:none;
+  }
+  .dialog>.footer>.right>w-button.main{
+    margin-left:16px;
   }
 `;
 class Dialog extends WComponent{
@@ -48,20 +69,37 @@ class Dialog extends WComponent{
     super();
   }
   init(){
-    let headProps={
-      className:"head",
-      textContent:this.title
-    };
-    if(headProps.textContent.trim().length===0){
-      headProps.className+=" head-empty";
-    }
-
     this.dialog=DOM.create("div", {props:{className:"dialog"}});
-    this.head=DOM.create("div", {props:headProps}, this.dialog);
+    this.head=DOM.create("div", {props:{className:"head"}}, this.dialog);
+    DOM.create("w-heading", {
+      attrs:{level:3},
+      props:{className:"title", textContent:this.title}
+    }, this.head);
+    DOM.create("div", {
+      props:{className:"close"}
+    }, this.head);
     this.main=DOM.create("slot", {}, this.dialog);
     this.footer=DOM.create("div", {props:{
       className:"footer"
     }}, this.dialog);
+    const footerLeft=DOM.create("div", {
+      props:{className:"left"}
+    }, this.footer);
+    DOM.create("w-button", {
+      attrs:{type:"link", size:"sm"},
+      props:{className:"noteless", textContent:"Cancel"}
+    }, footerLeft);
+    const footerRight=DOM.create("div", {
+      props:{className:"right"}
+    }, this.footer);
+    DOM.create("w-button", {
+      attrs:{type:"outline", size:"sm"},
+      props:{className:"secondary", textContent:"Cancel"}
+    }, footerRight);
+    DOM.create("w-button", {
+      attrs:{size:"sm"},
+      props:{className:"main", textContent:"OK"}
+    }, footerRight);
   }
   update({ name, newValue }){
     newValue=this.parseAttributeValueByName(name, newValue);
--- wc/theme/light.css.js
+++ wc/theme/light.css.js
@@ -64,6 +64,11 @@ const LightThemeStylesheet=`
 
   color: var(--color-gray-100);
 
+  /* Box Shadow */
+  --box-shadow-10:0px 1px 1px rgba(70, 73, 77, 0.2), 0px 1px 8px rgba(70, 73, 77, 0.1);
+  --box-shadow-20:0px 2px 2px rgba(70, 73, 77, 0.2), 0px 2px 12px rgba(70, 73, 77, 0.15);
+  --box-shadow-30:0px 3px 3px rgba(70, 73, 77, 0.2), 0px 3px 16px rgba(70, 73, 77, 0.2);
+
   /* Font Size */
   --font-size-small: 14px;
   --font-size-normal: 16px;
