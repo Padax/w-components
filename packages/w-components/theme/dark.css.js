021a12ac7929b58a056aebda16252f530f408fdb
Niz Kuo <nizniz187@gmail.com>
2022-08-14 18:32:56 +0800

    #147: Add shadow color for dark theme.

--- packages/w-components/theme/dark.css.js
+++ packages/w-components/theme/dark.css.js
@@ -64,6 +64,11 @@ const DarkThemeStylesheet=`
 
   color: var(--color-gray-100);
 
+  /* Box Shadow */
+  --box-shadow-10:0px 1px 1px rgba(0, 0, 0, 0.2), 0px 1px 8px rgba(0, 0, 0, 0.1);
+  --box-shadow-20:0px 0px 2px 2px rgba(0, 0, 0, 0.2), 0px 2px 12px rgba(0, 0, 0, 0.15);
+  --box-shadow-30:0px 0px 3px 3px rgba(0, 0, 0, 0.2), 0px 3px 16px rgba(0, 0, 0, 0.2);
+
   /* Font Size */
   --font-size-small: 14px;
   --font-size-normal: 16px;
